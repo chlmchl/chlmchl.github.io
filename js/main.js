eddf7f0c9842e33f691dd6ed5e9ac09a79940a95
chlmchl <ch.michel96@gmail.com>
2017-01-23 13:31:55 +0100

    fullpage

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['home', 'work']
+  });
+
+  $(".typed").typed({
+      strings: ["la street.", "ma gueule.", "bien ou bien ?"],
+      loop: true
+  });
+
+  $('.img-container img').each(function(index, item) {
+    $(item).css('transition-delay', index * 0.05 + 's');
+  })
+
+  $('.img-container img').hover(function(event) {
+    var src = $(event.target).attr('src');
+
+    $('.img-container img').css('opacity', '0');
+    $('.grid-background img').attr('src', src);
+  },
+  function(event) {
+    $('.img-container img').css('opacity', '1');
+  });
+});
