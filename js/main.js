96d43c2144a6308a32913577957a2238679df1bc
chlmchl <ch.michel96@gmail.com>
2017-07-05 19:32:34 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -15,5 +15,40 @@ function main() {
   $('p').fadeIn(1000);
 };
 
+$(document).ready(function() {
+  var scene = document.getElementById('parallax');
+  var parallax = new Parallax(scene, { 
+  calibrateX: false,
+    calibrateY: true,
+    invertX: true,
+    invertY: true,
+    limitX: false,
+    limitY: false,
+    scalarX: 2,
+    scalarY: 1,
+    frictionX: 0.1,
+    frictionY: 0.1,
+    originX: 0.1,
+    originY: 0.1,
+  });
+});
+
+$(document).ready(function() {
+  var scene = document.getElementById('parallax_0001');
+  var parallax = new Parallax(scene, { 
+  calibrateX: false,
+    calibrateY: true,
+    invertX: true,
+    invertY: true,
+    limitX: false,
+    limitY: false,
+    scalarX: 2,
+    scalarY: 1,
+    frictionX: 0.1,
+    frictionY: 0.1,
+    originX: 0.1,
+    originY: 0.1,
+  });
+});
 
 $(document).ready(main);
